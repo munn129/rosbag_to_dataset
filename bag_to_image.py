836cd321eabed2de8fd14ce024537a6c40a4ae64
moon <ans92175@gmail.com>
2024-08-29 09:15:02 +0900

    read bag messages

--- bag_to_image.py
+++ bag_to_image.py
@@ -13,16 +13,20 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
 def main():
-    BAG_FILE_PATH = 'debug.bag'
-    IMAGE_TOPIC = '/gmsl_camera/dev/video0/compressed'
-    GPS_TOPIC = '/novatel/oem7/inspva'
+    BAG_FILE_PATH = 'debug2.bag'
+    IMAGE_1_TOPIC = '/clpe_ros/cam_0/image_raw'
+    IMAGE_2_TOPIC = '/clpe_ros/cam_2/image_raw'
+    POSE_TOPIC = '/kiss/odometry'
     OUTPUT_DIR = './output'
+
+    topics = [POSE_TOPIC, IMAGE_1_TOPIC, IMAGE_2_TOPIC]
 
     bag = rosbag.Bag(BAG_FILE_PATH, "r")
     bridge = CvBridge()
     count = 0
-    for topic, msg, t in bag.read_messages(topics=IMAGE_TOPIC):
-        cv_img = bridge.compressed_imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    for topic, msg, time in bag.read_messages(topics=topics):
+        cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+        
 
         cv2.imwrite(os.path.join(OUTPUT_DIR, "frame%06i.png" % count), cv_img)
         print ("Wrote image %i" % count)
