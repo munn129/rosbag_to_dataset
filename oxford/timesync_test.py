99ac518a186e4b0401b5c562d9e067821d7a5be4
moon <ans92175@gmail.com>
2024-09-06 11:15:42 +0900

    time sync test

--- oxford/timesync_test.py
+++ oxford/timesync_test.py
@@ -5,6 +5,7 @@ ins_csv = '/media/moon/moon_ssd/moon ubuntu/oxford/0514/2014-05-14-13-46-12_gps/
 front_cam = '/media/moon/moon_ssd/moon ubuntu/oxford/0514/2014-05-14-13-46-12_stereo_centre_01/2014-05-14-13-46-12/stereo.timestamps'
 
 camera_time_list = []
+ins_time_list = []
 
 with open(front_cam, 'r') as file:
     for line in file:
@@ -13,10 +14,18 @@ with open(front_cam, 'r') as file:
 print(len(camera_time_list))
 
 with open(ins_csv, 'r') as file:
-    cnt = 0
-
     for line in file:
-        print(line)
-        cnt += 1
+        if line[0] == 't': continue
+        if line.split(',')[1] == 'INS_SOLUTION_GOOD':
+            ins_time_list.append(line.split(',')[0])
 
-        if cnt > 10: break
+print(len(ins_time_list))
+
+test = []
+for cam in camera_time_list:
+    for ins in ins_time_list:
+        if abs(int(cam) - int(ins)) < 10000:
+            test.append(cam)
+            break
+
+print(len(test))
