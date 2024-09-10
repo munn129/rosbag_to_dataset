dbba277adf246939d272f94c4a7f18185b20c1b6
moon <ans92175@gmail.com>
2024-09-10 10:12:16 +0900

    oxford dataset maker init

---
+++ oxford/oxford_dataset_maker.py
@@ -0,0 +1,13 @@
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+from sdk.camera_model import CameraModel
+from sdk.image import load_image
+
+import cv2
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
