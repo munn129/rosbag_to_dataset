3f7b7380b0ad54adc1b2baa1ae44c9a0e3fe34c3
moon <ans92175@gmail.com>
2024-10-02 20:50:33 +0900

    .

--- for_calibration/cam_lidar.py
+++ for_calibration/cam_lidar.py
@@ -9,9 +9,20 @@ import open3d as o3d
 import sensor_msgs.point_cloud2 as pc2
 from cv_bridge import CvBridge
 
-# def save_pcd(cloud, timestamp, path):
-#     p = pcl.PointCloud(np.array(list(pc2.read_points(cloud)), dtype=np.float32)[:, 0:3])
-#     p.to_file(path + '/pcd' + '_' + "{:.5f}".format(timestamp) + '.pcd')
+class Message:
+    def __init__(self, topic_name, msg, time) -> None:
+        self.topic_name = topic_name
+        self. msg = msg
+        self.time = time
+
+    def get_topic_name(self) -> str:
+        return str(self.topic_name)
+    
+    def get_msg(self):
+        return self.msg
+    
+    def get_time(self) -> int:
+        return int(str(self.time))
 
 def save_pcd (pcd_msg, path):
 
@@ -32,18 +43,40 @@ def main():
     left_image = '/stereo/left/image_color/compressed'
     right_image = '/stereo/right/image_color/compressed'
     lidar = '/lidar_points'
+
+    left_image_save_dir = '/home/moon/Documents/for_calibration/left_images'
+    right_image_save_dir = '/home/moon/Documents/for_calibration/right_images'
+    left_jpg = '/home/moon/Documents/for_calibration/left_jpg'
+    right_jpg = '/home/moon/Documents/for_calibration/right_jpg'
+    pcd_save_dir = '/home/moon/Documents/for_calibration/lidar_pcd'
     
     topics = [left_image, right_image, lidar]
 
     bag = rosbag.Bag(bag_file, 'r')
 
+    msg_dict = {
+        'lidar' : [],
+        'left_cam' : [],
+        'right_cam' : []
+    }
+
     cnt = 0
 
-    for topic, msg, time in bag.read_messages(topics = topics):
-        print(f'time: {time}, topic: {topic}')
-        if topic == lidar:
-            save_pcd(msg, f'{cnt:06d}.pcd')
-            cnt += 1
+    tmp = []
 
+    critia = 10000000
+
+    for topic, msg, time in bag.read_messages(topics = topics):
+        # print(f'time: {time}, topic: {topic}')
+        
+        if topic == lidar:
+            msg_dict['lidar'].append(Message(topic, msg, time))
+        elif topic == left_image:
+            msg_dict['left_cam'].append(Message(topic, msg, time))
+        elif topic == right_image:
+            msg_dict['right_cam'].append(Message(topic, msg, time))
+        else:
+            print('topic name is not matched')
+        
 if __name__ == '__main__':
     main()
