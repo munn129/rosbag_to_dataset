62ace74e2fb4de4a79349e6f773ffc13bca8c5f3
moon <ans92175@gmail.com>
2024-10-04 14:33:56 +0900

    bag to jpg

--- for_calibration/cam.py
+++ for_calibration/cam.py
@@ -20,12 +20,38 @@ def main():
 
     bag = rosbag.Bag(bag_file, 'r')
 
-    left_list = []
-    right_list = []
+    bridge = CvBridge()
 
+    r_count = 0
+    l_count = 0
+
+    init_time = 0
 
     for topic, msg, time in bag.read_messages(topics = topics):
-        pass
+
+        time = int(str(time))
+
+        if time - init_time > 1000000000:
+        
+            if topic == left_image:
+                img = bridge.compressed_imgmsg_to_cv2(msg, desired_encoding='passthrough')
+
+                cv2.imwrite(f'{left_jpg}/{l_count:06d}.jpg', img)
+
+                print(f'{left_jpg}/{l_count:06d}.jpg is saved')
+
+                l_count += 1
+
+            if topic == right_image:
+                img = bridge.compressed_imgmsg_to_cv2(msg, desired_encoding='passthrough')
+
+                cv2.imwrite(f'{right_jpg}/{r_count:06d}.jpg', img)
+
+                print(f'{right_jpg}/{r_count:06d}.jpg is saved')
+
+                r_count += 1
+
+            init_time = time
 
 if __name__ == '__main__':
     main()
